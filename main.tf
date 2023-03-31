66713027fb2205dcae033609cb35e658070ef44d
Tobias Effner <tobias.effner@rackspace.com>
2023-03-31 09:59:41 +0200

    Added action file

--- main.tf
+++ main.tf
@@ -20,25 +20,40 @@
 # }
 
 module "tobiasmichael_zone" {
-    source = "./route53"
-    domain = "tobiasmichael.de"
-    additional_records = [
-        {
-            name = "tobiasmichael.de"
-            type = "MX"
-            ttl  = "150"
-            records = ["5 smtpin.rzone.de"]
-        },
-    ]
+  source = "./route53"
+  domain = "tobiasmichael.de"
+  additional_records = [
+    {
+      name    = "tobiasmichael.de"
+      type    = "MX"
+      ttl     = "150"
+      records = ["5 smtpin.rzone.de"]
+    },
+  ]
 }
 
 module "tobiasmichael_website" {
-    source = "./website"
-    route53_hosted_zone_id = module.tobiasmichael_zone.zone_id
-    website_domain_name = "tobiasmichael.de"
+  source                 = "./website"
+  route53_hosted_zone_id = module.tobiasmichael_zone.zone_id
+  website_domain_name    = "tobiasmichael.de"
 
-    providers = {
-        aws = aws, 
-        aws.certificate = aws.certificate
+  cloudfront_custom_error_responses = [
+    {
+      error_caching_min_ttl = 30,
+      error_code            = 403,
+      response_code         = 200,
+      response_page_path    = "error.html"
+    },
+    {
+      error_caching_min_ttl = 30,
+      error_code            = 404,
+      response_code         = 200,
+      response_page_path    = "error.html"
     }
+  ]
+
+  providers = {
+    aws             = aws,
+    aws.certificate = aws.certificate
+  }
 }
